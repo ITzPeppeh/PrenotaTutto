2a3bea4e86b0e3427de4d8947509a626622596a2
Giuseppe <loggaisreal@gmail.com>
2023-04-28 18:16:14 +0200

    Database Done

--- database.sql
+++ database.sql
@@ -1,26 +1,28 @@
 CREATE DATABASE IF NOT EXISTS divinodoni_prenotazioni;
 
 USE divinodoni_prenotazioni;
---TO FINISH
+
 CREATE TABLE IF NOT EXISTS attivita (
-    'CodA',
-    'NomeA',
-    'MaxPosti',
-    'PostiPren',
+    CodA VARCHAR(100),
+    NomeA VARCHAR(100) NOT NULL,
+    MaxPosti INT(100) NOT NULL,
+    PostiPren INT(100) NOT NULL,
     PRIMARY KEY (CodA)
-)
+);
 
 CREATE TABLE IF NOT EXISTS utente (
-    'Username',
-    'Password',
-    'Cognome',
-    'Nome',
+    Username VARCHAR(50),
+    Passwd VARCHAR(50) NOT NULL,
+    Cognome VARCHAR(100) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
     PRIMARY KEY (Username)
-)
+);
 
 CREATE TABLE IF NOT EXISTS prenota (
-    'CodA',
-    'Username',
-    'Persone',
-    PRIMARY KEY ()
-)
+    CodA VARCHAR(100),
+    Username VARCHAR(50),
+    Persone INT(5) DEFAULT 1,
+    PRIMARY KEY (CodA, Username),
+    FOREIGN KEY (CodA) REFERENCES attivita(CodA),
+    FOREIGN KEY (Username) REFERENCES utente(Username)
+);
